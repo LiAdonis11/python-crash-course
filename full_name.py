501875c09abb544d9e947a3da4772fa0ea41d362
LiAdonis11 <leeadonislucero11@gmail.com>
2026-08-26 20:39:38 +0800

    String Manipulation

---
+++ full_name.py
@@ -0,0 +1,15 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}"
+print(message)
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages: \n\tPython\n\tC\n\tJavaScript")
+favorite_language = '   python   '
+
+print(favorite_language.strip())
+
+nostarch_url = 'https://nostarch.com'
+simple_url = nostarch_url.removeprefix('https://')
+print(simple_url)
---
+++ name_cases.py
@@ -0,0 +1,21 @@
+name = "Gors gorio"
+question = "are you having fun?"
+full_message = f'{name}, {question}'
+print(full_message)
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+quote = 'once said, "Success is not final, failure is not fatal: it is the courage to continue that counts"'
+famous_name = "Winston Churchill"
+message = f"{famous_name} {quote}"
+print(message)
+
+richest_man = "\tElon \n\tMask"
+print(richest_man)
+print(richest_man.lstrip())
+print(richest_man.rstrip())
+print(richest_man.strip())
+
+filename = "python_notes.txt"
+print(filename.removesuffix('.txt'))
