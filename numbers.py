3759be686d6811d821ca4145f665800c04273f0b
LiAdonis11 <leeadonislucero11@gmail.com>
2026-08-28 06:16:04 +0800

    Learning numbers

---
+++ numbers.py
@@ -0,0 +1,11 @@
+x = 5
+y = 3
+
+print(x + y)
+print(13 - x)
+print(2 * 4)
+print(int(16 / 2))
+
+number = 11
+message = "its my birthday"
+print(f"My favorite number is {number} becuase {message}")
