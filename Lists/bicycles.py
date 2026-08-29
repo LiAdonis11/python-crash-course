988a67bd47c680c00decd388b0f6c1a30ff66230
LiAdonis11 <leeadonislucero11@gmail.com>
2026-08-29 19:59:14 +0800

    Learn how to use a Lists

---
+++ Lists/bicycles.py
@@ -0,0 +1,8 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized', 'Foxter']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[2].title())
+print(bicycles[-1].upper())
+
+message = f"My first bicycles was a {bicycles[-1].title()}."
+print(message)
---
+++ Lists/names.py
@@ -0,0 +1,17 @@
+names = ["Carlo", "Benito", "Foster", "Dane","Deo", "Michelle"]
+message = "Hoping you find a job suited to you my friend"
+print(f"{message}, {names[0]}.")
+print(f"{message}, {names[1]}.")
+print(f"{message}, {names[2]}.")
+print(f"{message}, {names[3]}.")
+print(f"{message}, {names[4]}.")
+print(f"{message}, {names[5]}.")
+print("\n")
+transportation = ["bike", "motorcycle","E-bike"]
+
+bike = f"I would like to buy a {transportation[0]} a mountainpeak, its a good excercise and not that expensive"
+motorcycle = f"I'm using the {transportation[1]} of my father as a mode of transportation."
+ebike = f"One day i would like to buy {transportation[2]}, its a good for strolling."
+print(bike)
+print(motorcycle)
+print(ebike)
