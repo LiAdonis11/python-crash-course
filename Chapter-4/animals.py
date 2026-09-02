166583ee00e3056669badfa7e9e3e8aa7b25b4bc
LiAdonis11 <leeadonislucero11@gmail.com>
2026-09-02 17:53:40 +0800

    Learned how to use loop

---
+++ Chapter-4/animals.py
@@ -0,0 +1,4 @@
+animals = ['dog', 'tiger', 'lion']
+for animal in animals:
+    print(f'A {animal} make a good guard.')
+print("Any of these animals make a good guard becuase they are strong.")
---
+++ Chapter-4/pizzas.py
@@ -0,0 +1,4 @@
+pizzas = ['bbq', 'hawaiian', 'pepperoni']
+for pizza in pizzas:
+    print(f'I like to eat {pizza.title()} pizza.')
+print("Its one of my favorite snacks.")
