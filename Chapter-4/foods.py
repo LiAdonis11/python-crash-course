ef86abe350cb636309344538e7e3931e2595afa6
LiAdonis11 <leeadonislucero11@gmail.com>
2026-09-07 21:20:06 +0800

    Another loop and list activity

--- Chapter-4/foods.py
+++ Chapter-4/foods.py
@@ -4,7 +4,13 @@ friend_foods = my_foods
 my_foods.append('cannoli')
 print("My favorite foods are:")
 print(my_foods)
+print("-----------------------------------------------")
+for food in my_foods:
+    print(food)
 
 friend_foods.append("ice cream")
 print("\nMy friend's favorite foods are:")
 print(friend_foods)
+print("-----------------------------------------------")
+for food in friend_foods:
+    print(food)
