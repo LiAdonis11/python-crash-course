e49684e25d273982e059aadc5dba766d4e8c6020
LiAdonis11 <leeadonislucero11@gmail.com>
2026-09-07 21:15:41 +0800

    Copy list activity

--- Chapter-4/pizzas.py
+++ Chapter-4/pizzas.py
@@ -3,3 +3,26 @@ for pizza in pizzas:
     print(f'I like to eat {pizza.title()} pizza.')
 print("Its one of my favorite snacks.")
 
+friend_pizzas = pizzas[:]
+print(pizzas)
+print(friend_pizzas)
+
+
+pizzas.append('ham and cheese')
+friend_pizzas.append('spicy fish')
+
+print("\n")
+
+print(pizzas)
+print(friend_pizzas)
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("\n")
+
+print("My friends's favorite pizza are:")
+for pizza in friend_pizzas:
+    print(pizza)
+    
