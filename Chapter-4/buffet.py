31db89a52cfa9f5642749f053a31c66cea49c383
LiAdonis11 <leeadonislucero11@gmail.com>
2026-09-09 10:20:04 +0800

    Learn how to use tuples

---
+++ Chapter-4/buffet.py
@@ -0,0 +1,7 @@
+buffets = ('sausage', 'letchon', 'bbq', 'ham', 'chicken joy')
+for buffet in buffets:
+    print(buffet)
+
+buffets = ('fishball', 'letchon', 'bbq', 'longganisa', 'chicken joy')
+for buffet in buffets:
+    print(buffet)
