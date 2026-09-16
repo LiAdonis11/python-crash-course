f0dd6285869c62bb078cd18b1deec65df43e88ca
LiAdonis11 <leeadonislucero11@gmail.com>
2026-09-17 04:50:59 +0800

    Lots of if elif else activity

---
+++ Chapter-5/activity/favorite_food.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['banana', 'apple', 'mangoe']
+
+if 'mangoe' in favorite_fruits:
+    print("You really like mangoes!")
+if 'avocado' in favorite_fruits:
+    print("You really like avocados!")
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+if 'strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
