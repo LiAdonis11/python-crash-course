571963593b922990e813edd364961a60a9ecd0ca
LiAdonis11 <leeadonislucero11@gmail.com>
2026-09-21 18:13:36 +0800

    If statements in alist

--- Chapter-5/toppings.py
+++ Chapter-5/toppings.py
@@ -1,10 +1,9 @@
-requested_topping = ['mushrooms', 'extra cheese']
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
-if 'mushrooms' in requested_topping:
-    print("Adding anchovies.")
-if 'pepperoni' in requested_topping:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_topping:
-    print("Adding extra cheese")
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Where out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}")
 
 print("\nFinished making your pizza!")
