95c488731c57a91a96ba0300129cee19a9e01987
LiAdonis11 <leeadonislucero11@gmail.com>
2026-09-21 20:26:03 +0800

    combining conditional statements with a loop and list

--- Chapter-5/toppings.py
+++ Chapter-5/toppings.py
@@ -1,9 +1,19 @@
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+# requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+# for requested_topping in requested_toppings:
+#     if requested_topping == 'green peppers':
+#         print("Where out of green peppers right now.")
+#     else:
+#         print(f"Adding {requested_topping}")
+
+# print("\nFinished making your pizza!")
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms','extra cheese','french fries']
 
 for requested_topping in requested_toppings:
-    if requested_topping == 'green peppers':
-        print("Where out of green peppers right now.")
-    else:
+    if requested_topping in available_toppings:
         print(f"Adding {requested_topping}")
+    else: print(f"Sorry, we dont have {requested_topping}")
 
 print("\nFinished making your pizza!")
